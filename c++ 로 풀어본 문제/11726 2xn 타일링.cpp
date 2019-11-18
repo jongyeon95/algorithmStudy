dd17f105c26d310ddbffae3526750db18abb613d
whddus9509 <whddus9509@naver.com>
2019-11-18 14:45:10 +0900

    upup

---
+++ c++ 로 풀어본 문제/11726 2xn 타일링.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[1001]={0,1,2};
+	int n;
+	cin >> n;
+	if(arr[n]==0){
+		for (int i = 3; i <=n; i++)
+		{
+			arr[i]=arr[i-1]+arr[i-2];
+			arr[i]%=10007;
+		}
+	}
+	cout << arr[n];
+
+}
