634006e2f914acc617edd5ada9399fd2458e1785
whddus9509 <whddus9509@naver.com>
2019-11-24 23:11:17 +0900

    Create 11650 좌표 정렬하기.cpp

---
+++ c++ 로 풀어본 문제/11650 좌표 정렬하기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool cmp(pair<int,int> p1, pair<int,int> p2){
+	if(p1.first==p2.first)
+		return p1.second< p2.second;
+	return p1.first < p2.first;
+}
+
+int main(){
+	int n;
+	vector<pair<int,int>> v;	
+	cin >> n;
+	while(n--){
+		int a,b;
+		cin >> a >> b;
+		v.push_back(pair<int,int>(a,b));
+	}
+	sort(v.begin(),v.end(),cmp);
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i].first << " "<< v[i].second <<"\n"; 
+	}
+
+}
