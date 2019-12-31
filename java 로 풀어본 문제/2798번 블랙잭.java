ec4e942537f6a5465af34b565aa7ca55d5e5987a
whddus9509 <whddus9509@naver.com>
2019-12-31 20:36:30 +0900

    java도 해보자..

---
+++ java 로 풀어본 문제/2798번 블랙잭.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		Scanner sc= new Scanner(System.in);
+		int n,m;
+		n=sc.nextInt();
+		m=sc.nextInt();
+		int[] array=new int[n];
+		for (int i=0; i<n; i++) {
+			array[i]=sc.nextInt();
+		}
+		int result=0;
+		for (int i=0; i<n-2; i++) {
+			for (int j=i+1; j<n-1;j++) {
+				for (int k=j+1; k<n;k++ ) {
+					int temp=array[i]+array[j]+array[k];
+					if(temp<=m&&result<temp){
+						result=temp;	
+					}
+				}
+			}
+		}
+
+		System.out.print(result);
+
+	}
+}
