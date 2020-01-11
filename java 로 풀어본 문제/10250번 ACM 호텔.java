e3131f828af456f1e4b5b1988323e604e84a82fc
whddus9509 <whddus9509@naver.com>
2020-01-12 01:16:10 +0900

    Create 10250번 ACM 호텔.java

---
+++ java 로 풀어본 문제/10250번 ACM 호텔.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) throws Exception{
+		Scanner s=new Scanner(System.in);
+		int testcase=s.nextInt();
+		while(testcase>0){
+			int h,w,n;
+			h=s.nextInt();
+			w=s.nextInt();
+			n=s.nextInt();
+			n--;
+			System.out.println((n/h+1)+((n%h+1)*100));
+			testcase--;
+		}
+	}
+}
