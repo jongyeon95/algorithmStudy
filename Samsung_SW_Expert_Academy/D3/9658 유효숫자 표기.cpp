28387d1a00d7e2f5d34830167d82a889d04d4fdc
whddus9509 <whddus9509@naver.com>
2020-03-16 00:54:28 +0900

    Update 9658 유효숫자 표기.cpp

--- Samsung_SW_Expert_Academy/D3/9658 유효숫자 표기.cpp
+++ Samsung_SW_Expert_Academy/D3/9658 유효숫자 표기.cpp
@@ -2,47 +2,41 @@
 #include <string>
 using namespace std;
 int main(){
-	cin.tie(0);
-	cout.sync_with_stdio(false);
-	int testcase;
-	cin >> testcase;
-	for (int t = 1; t <= testcase; ++t)
-	{
-		string s;
-		cin >> s;
-		int arr[4];
-		int size=s.size()-1;
-		int outputFront;
-		int outputBack;
-		bool check=false;
-		arr[0]=0;
-		for (int i = 1; i < 4; ++i)
-		{
-			arr[i]=s[i-1]-'0';
-		}
-		if(arr[3]>4){
-			arr[2]+=1;
-			if(arr[2]==10){
-				arr[2]=0;
-				arr[1]+=1;
-				if(arr[1]==10){
-					arr[1]=0;
-					arr[0]=1;
-					outputFront=1;
-					outputBack=0;
-					check=true;
-					size+=1;
-				}
-
-			}
-		}
-		if(!check){
-			outputFront=arr[1];
-			outputBack=arr[2];
-		}
-		cout<<"#"<<t<<" "<<outputFront<<"."<<outputBack<<"*10^"<<size<<"\n";
-
-
-	}
-
+    cin.tie(0);
+    cout.sync_with_stdio(false);
+    int testcase;
+    cin >> testcase;
+    for (int t = 1; t <= testcase; ++t)
+    {
+        string s;
+        cin >> s;
+        int arr[3];
+        int size=s.size()-1;
+        bool check=false;
+        arr[0]=0;
+        for (int i = 0; i < 3; ++i)
+        {
+            arr[i]=s[i]-'0';
+        }
+        if(arr[2]>4){
+            arr[1]+=1;
+            if(arr[1]==10){
+                arr[0]+=1;
+                if(arr[0]==10){
+                    arr[0]=1;
+                    arr[1]=0;
+                    check=true;
+                    size+=1;
+                }
+            }
+        }
+        if(!check){
+            if(arr[1]==10)
+                arr[1]=0;
+        }
+        printf("#%d %d.%d*10^%d\n",t,arr[0],arr[1],size);
+ 
+ 
+    }
+ 
 }
