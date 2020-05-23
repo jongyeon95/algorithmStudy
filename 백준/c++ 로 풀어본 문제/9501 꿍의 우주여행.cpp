6ee284b81864aae411845cedaaac2ca81807c660
jongyeon <whddus9509@naver.com>
2020-05-23 21:34:16 +0900

    Create 9501 꿍의 우주여행.cpp

---
+++ 백준/c++ 로 풀어본 문제/9501 꿍의 우주여행.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, d, ans = 0;
+        cin >> n >> d;
+        while (n--) 
+        {
+            int v, f, c;
+            cin >> v >> f >> c;
+          
+            if (v * f / c>=d)
+                    ans++;
+        }
+        cout << ans << '\n';
+    }
+}
+
