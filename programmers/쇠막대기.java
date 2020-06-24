aea1357752820abbf9f854a9f28225775b0e5a3b
jongyeon <whddus9509@naver.com>
2020-06-24 16:19:42 +0900

    Create 쇠막대기.java

---
+++ programmers/쇠막대기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String arrangement) {
+        int answer = 0;
+        int flag = 0;
+        for(int i=0; i<arrangement.length(); i++){
+            if(arrangement.charAt(i)=='('){
+                flag++;
+            }
+            else {
+                flag--;
+                if(arrangement.charAt(i-1)=='(')
+                    answer+= flag;
+                else
+                    answer +=1;
+            }
+        }
+        return answer;
+    }
+}
