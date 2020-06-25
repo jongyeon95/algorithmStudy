21db17f4f58346b5dc912b1c17528a3386a4910e
jongyeon <whddus9509@naver.com>
2020-06-25 17:28:44 +0900

    Create 같은 숫자는 싫어.java

---
+++ programmers/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> a= new ArrayList<Integer>();
+        for(int i=0; i<arr.length-1;i++){
+           if(arr[i]!=arr[i+1]){
+               a.add(arr[i]);
+           }
+        }
+      
+        a.add(arr[arr.length-1]);
+        
+        int[] answer = new int[a.size()];
+       
+        for(int i=0; i<a.size();i++){
+           answer[i]=a.get(i);
+       }
+
+
+        return answer;
+    }
+}
