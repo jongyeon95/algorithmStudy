77af6e1c5ca4e47ec3b6ff7eab7e55f5acc81130
jongyeon <whddus9509@naver.com>
2020-07-01 02:07:04 +0900

    Create 위장.java

---
+++ programmers/위장.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+    	int answer = 1; 
+
+    	HashMap<String, Integer> map = new HashMap<>();
+    	
+    	for(int i = 0; i < clothes.length; i++) {
+    		if(map.get(clothes[i][1]) == null)
+    			map.put(clothes[i][1], 1);
+    		else
+    			map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+    	}
+    	
+    	for(String keys: map.keySet()) {
+    		answer *= (map.get(keys) + 1);
+    	}
+        
+    	answer -= 1;
+        
+        return answer;
+    }
+}
