211014a315226964026e37e80de2b99a69963098
whddus9509 <whddus9509@naver.com>
2020-07-01 14:04:31 +0900

    Update 1926. 간단한 369게임.cpp

--- Samsung_SW_Expert_Academy/D2/1926. 간단한 369게임.cpp
+++ Samsung_SW_Expert_Academy/D2/1926. 간단한 369게임.cpp
@@ -4,24 +4,21 @@ using namespace std;
 int main(int argc, char** argv)
 {
     int n;
-	cin >> n;
+	scanf("%d",&n);
     for(int i=1; i<=n; i++){
         string s=to_string(i);
         int flag=0;
         for(int j=0; j<s.size(); j++){
-            if(s[j]=='3'||s[j]=='6'||s[j]=='9'){
-            	flag++;
-            }
+            if(s[j]=='3'||s[j]=='6'||s[j]=='9')
+            	flag++;          
         }
-        if(flag!=0){
-            for(int j=0; j<flag; j++){
-                cout<<'-';
-            }
-        }
-        else{
-        	cout<<s;
-        }
-        cout<< " ";
+        if(flag!=0)
+            for(int j=0; j<flag; j++)
+                printf("-");
+    
+        else
+        	printf("%d",i);
+        printf(" ");
     }
 	return 0;
 }
