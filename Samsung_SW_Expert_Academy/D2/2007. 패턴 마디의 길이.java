a20e49a0a366fd6c9f5261d8d017ae2175e409c4
whddus9509 <whddus9509@naver.com>
2020-07-01 15:11:06 +0900

    Create 2007. 패턴 마디의 길이.java

---
+++ Samsung_SW_Expert_Academy/D2/2007. 패턴 마디의 길이.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String s = sc.next();
+            int L=2;
+            while(true){
+                Boolean check=true;
+                 for(int i=0; i<30-L; i++){
+                     if(s.charAt(i)!=s.charAt(i+L)){
+                         check=false;
+                         break;
+                     }      
+           		 }
+                if(check){
+                	System.out.println("#"+test_case+" "+L);
+                    break;
+                }
+                else
+                    L++;
+            }
+		
+		}
+	}
+}
