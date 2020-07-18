7b0895e0e13b97f231b5a0522bc65220e4a46703
jongyeon <whddus9509@naver.com>
2020-07-19 00:38:26 +0900

    Update 1948. 날짜 계산기.cpp

--- Samsung_SW_Expert_Academy/D2/1948. 날짜 계산기.cpp
+++ Samsung_SW_Expert_Academy/D2/1948. 날짜 계산기.cpp
@@ -18,10 +18,9 @@ int main(int argc, char** argv)
         int result=0;
         cin >> M1 >> D1>> M2 >> D2;
         
-        result=day[M1]-D1+1;// 시작 월 
-        for(int i=M1+1; i<=M2; i++){
-            if(i==M2){// 마지막 월은 입력값만 더한다.
-            	result+=D2;
+        for(int i=M1; i<=M2; i++){
+            if(i==M2){// 마지막 월은 D2입력값을 더하고 D1 값을 빼준다.
+            	result+=D2-D1+1;
                 continue;
             }
         	result+=day[i];
