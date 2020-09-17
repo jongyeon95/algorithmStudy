3e7b8fb349d5ae30e69cf6f0caf1a6921b90f406
jongyeon <whddus9509@naver.com>
2020-09-17 21:28:54 +0900

    Create 두 개 뽑아서 더하기.java

---
+++ programmers/두 개 뽑아서 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        
+        ArrayList<Integer> al=new ArrayList<>();
+        TreeSet<Integer> t=new TreeSet<>();
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                t.add(numbers[i]+numbers[j]);
+            }
+        }
+        for(Integer a : t){
+            al.add(a);
+        }
+        int[] answer = new int[al.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i]=al.get(i);
+        }
+      
+        return answer;
+    }
+}
