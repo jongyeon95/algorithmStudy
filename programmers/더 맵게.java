85d46e328d74077037c77007e2162848d5997955
jongyeon <whddus9509@naver.com>
2020-10-01 10:02:32 +0900

    Create 더 맵게.java

---
+++ programmers/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i=0; i<scoville.length;i++){
+            q.offer(scoville[i]);
+        }
+        while(true){
+            if(q.isEmpty()){
+                return -1;
+            }
+            int temp=q.poll();
+            if(temp<K){
+                if(q.isEmpty()){
+                    return -1;
+                }
+                temp+=q.poll()*2;
+                q.offer(temp);
+                answer++;
+            }
+            else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
