a5ad96424c7aa9522d84f7d30e74c47356a809ef
jongyeon <whddus9509@naver.com>
2020-10-04 20:36:43 +0900

    Create 타겟 넘버.java

---
+++ programmers/타겟 넘버.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int target;
+    public int size;
+    public int answer;
+    
+    public void dfs(int cnt, int num,int[] numbers){
+        if(cnt==size){
+            if(num==target){
+                answer++;
+            }
+            return;
+        }
+        dfs(cnt+1,num+numbers[cnt],numbers);
+        dfs(cnt+1,num-numbers[cnt],numbers);
+    }
+    
+    
+    public int solution(int[] numbers, int target) {
+        this.answer = 0;
+        this.target=target;
+        this.size=numbers.length;
+        dfs(0,0,numbers);
+        return this.answer;
+    }
+}
