b166b216bdf7cc62c1ea64bbeceda05b2fd6b29e
jongyeon <whddus9509@naver.com>
2020-10-10 10:36:40 +0900

    Create 올바른 괄호.java

---
+++ programmers/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Queue<Character> q=new LinkedList<Character>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                q.offer('(');
+            }
+            else{
+                if(q.size()==0)
+                    return false;
+                else
+                    q.poll();
+            }
+        }
+        if(q.size()>0)
+            return false;
+
+        return answer;
+    }
+}
