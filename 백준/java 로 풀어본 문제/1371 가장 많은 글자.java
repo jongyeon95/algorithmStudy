6944ad1e7fb801a89e2dbe25b1900b1e831e52c9
jongyeon <whddus9509@naver.com>
2020-12-31 14:16:19 +0900

    Create 1371 가장 많은 글자.java

---
+++ 백준/java 로 풀어본 문제/1371 가장 많은 글자.java
@@ -0,0 +1,32 @@
+import java.io.*; 
+import java.util.*;
+
+
+
+public class Main {
+    public static StringBuffer n;
+
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+        int alpha[] = new int[26];
+        int max = 0;
+        while(sc.hasNextLine()){
+            String line = sc.nextLine();
+            for(int i = 0; i < line.length(); i++){
+                if(line.charAt(i) != ' '){
+                    alpha[line.charAt(i) - 'a']++;
+                    if(max < alpha[line.charAt(i) - 'a']){
+                        max = alpha[line.charAt(i) - 'a'];
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] == max) System.out.print((char)(i + 'a'));
+        }
+
+    }
+
+
+}
+
