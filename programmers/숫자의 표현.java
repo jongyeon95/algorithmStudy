a6b70cdc9a2192fa569fa9f8d65508b9fd54c753
jongyeon <whddus9509@naver.com>
2021-01-10 16:30:44 +0900

    Create 숫자의 표현.java

---
+++ programmers/숫자의 표현.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num=0;
+        int minusNum=1;
+        int addNum=1;
+        while(!(minusNum>=n&&addNum>=n)){
+            if(num==n){
+                answer++;
+                if(addNum>n)
+                    continue;
+                num+=addNum;
+                addNum++;
+            }
+            else if(num<n){
+                if(addNum>n)
+                    continue;
+                num+=addNum;
+                addNum++;
+            }
+            else{
+                if(minusNum>n)
+                    continue;
+                num-=minusNum;
+                minusNum++;
+            }
+        }
+        return answer+1;
+    }
+}
