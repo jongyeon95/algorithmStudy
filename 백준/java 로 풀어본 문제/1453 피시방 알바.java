848bf7d9b7d6cbb10ef4691c730e4729fb723fd2
jongyeon <whddus9509@naver.com>
2021-01-17 16:20:35 +0900

    Create 1453 피시방 알바.java

---
+++ 백준/java 로 풀어본 문제/1453 피시방 알바.java
@@ -0,0 +1,20 @@
+import java.io.*; 
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0; i<n; i++){
+            hs.add(Integer.valueOf(stk.nextToken()));
+        }
+        System.out.println(n-hs.size());
+        
+
+    }
+
+}
+
