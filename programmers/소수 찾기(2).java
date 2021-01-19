9cec63059575972d7e3362016e46126a84a9a45f
jongyeon <whddus9509@naver.com>
2021-01-19 11:27:36 +0900

    Create 소수 찾기(2).java

---
+++ programmers/소수 찾기(2).java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public static boolean[] num = new boolean[1000001];
+    
+    
+    public int solution(int n) {
+        int answer =n-findPn(n);
+        return answer;
+    }
+    
+    public int findPn(int n){
+        int answer=1;
+        for(int i=2; i<=n/2; i++){
+            if(!num[i]){
+                for(int j=i*2; j<=n; j+=i){
+                    if(num[j])
+                        continue;
+                    num[j]=true;
+                    answer++;
+                }
+            }
+        }
+        return answer;
+       
+    }
+}
