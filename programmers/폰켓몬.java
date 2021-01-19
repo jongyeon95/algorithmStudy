055ffb0ce819e6c2fd6d257b5c02ae383313e459
jongyeon <whddus9509@naver.com>
2021-01-19 11:09:31 +0900

    Create 폰켓몬.java

---
+++ programmers/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> hs =new HashSet<>();
+        answer=nums.length/2;
+        for(int num : nums){
+            hs.add(num);
+        }
+        if(hs.size()<answer)
+            answer=hs.size();
+        return answer;
+    }
+}
