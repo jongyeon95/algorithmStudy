07aab86828de73a9b6bd66bd2de0f85faae388e5
jongyeon95 <31908589+jongyeon95@users.noreply.github.com>
2021-02-02 19:16:43 +0900

    Create 서울에서 김서방 찾기.java

---
+++ 백준/java 로 풀어본 문제/서울에서 김서방 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String[] seoul) {
+       for(int i=0; i<seoul.length; i++){
+           if(seoul[i].equals("Kim"))
+               return "김서방은 "+i+"에 있다";
+       }
+        return null;
+    }
+}
