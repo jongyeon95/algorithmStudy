f4fbf233b1da053e1562e921b0ad5969177510e7
jongyeon95 <31908589+jongyeon95@users.noreply.github.com>
2021-02-06 18:22:22 +0900

    Create 다음 큰 숫자.java

---
+++ programmers/다음 큰 숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int bitCount = Integer.bitCount(n);
+        for(int i = n+1; ;i++) {
+            if(bitCount == Integer.bitCount(i)) {
+                return i;
+            }
+        }
+    }
+}
