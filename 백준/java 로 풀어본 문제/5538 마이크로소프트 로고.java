4e50150f3284528b697210af0207b229e4d437f3
jongyeon95 <31908589+jongyeon95@users.noreply.github.com>
2021-04-26 20:59:44 +0900

    Create 5538 마이크로소프트 로고.java

---
+++ 백준/java 로 풀어본 문제/5538 마이크로소프트 로고.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		bw.write("       _.-;;-._\n");
+		bw.write("'-..-'|   ||   |\n");
+		bw.write("'-..-'|_.-;;-._|\n");
+		bw.write("'-..-'|   ||   |\n");
+		bw.write("'-..-'|_.-''-._|");
+		bw.flush();
+		bw.close();
+		return;
+
+	}
+	
+	
+}
+
