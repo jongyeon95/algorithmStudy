e6b9e2367e812aea337a80824234b641299ea76f
jongyeon95 <31908589+jongyeon95@users.noreply.github.com>
2021-05-14 02:45:45 +0900

    Create 피보나치 수.java

---
+++ programmers/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        long[] arr= new long[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2; i<=n; i++){
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
+        }
+        answer=(int)arr[n];
+        return answer;
+    }
+}
